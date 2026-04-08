3b2006c944fda62de98b3873aa30fac7d8b4827c
oleg <osn298@gmail.com>
2026-04-08 22:22:29 +0300

    reload command

--- src/main/java/de/nikey/combatLog/CombatLog.java
+++ src/main/java/de/nikey/combatLog/CombatLog.java
@@ -7,12 +7,20 @@ import de.nikey.combatLog.Utils.Metrics;
 import de.nikey.combatLog.Utils.ModrinthUpdateChecker;
 import de.nikey.combatLog.Utils.WorldGuardBridge;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public final class CombatLog extends JavaPlugin {
 
     private CombatManager combatManager;
+    private PluginConfig pluginConfig;
     private WorldGuardBridge worldGuardBridge = null;
 
     @Override
@@ -34,7 +42,7 @@ public final class CombatLog extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
 
-        PluginConfig pluginConfig = new PluginConfig(getConfig());
+        pluginConfig = new PluginConfig(getConfig());
         combatManager = new CombatManager(this, pluginConfig);
 
         registerListeners(pluginConfig);
@@ -46,6 +54,46 @@ public final class CombatLog extends JavaPlugin {
     @Override
     public void onDisable() {
         combatManager.shutdown();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission("combatlog.reload")) {
+                sender.sendMessage("§cYou don't have permission to use this command.");
+                return true;
+            }
+            reloadConfig();
+            pluginConfig = new PluginConfig(getConfig());
+
+            // Preserve existing combat state
+            Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
+            ArrayList<Player> inCombat = new ArrayList<>();
+            for (Player p : onlinePlayers) {
+                if (combatManager.isInCombat(p)) {
+                    inCombat.add(p);
+                }
+            }
+
+            combatManager.shutdown();
+            combatManager = new CombatManager(this, pluginConfig);
+
+            // Re-register listeners with new config
+            HandlerList.unregisterAll(this);
+            registerListeners(pluginConfig);
+
+            // Restore combat tags for players who were in combat
+            for (Player player : inCombat) {
+                if (player.isOnline()) {
+                    combatManager.tag(player);
+                }
+            }
+
+            sender.sendMessage("§aCombatLog config reloaded successfully!");
+            getLogger().info("Config reloaded by " + sender.getName());
+            return true;
+        }
+        return false;
     }
 
     private void registerListeners(PluginConfig config) {
